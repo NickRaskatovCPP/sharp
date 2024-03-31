c198a1340174405123bbbf0443bbb509f544cee0
Raskatov <cppgod@ya.ru>
2024-03-31 17:54:54 +0300

    first try c#

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cpppp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float result;
+            int x, y;
+            int timeInMinuets = 123765542;
+            int hour;
+            int minute;
+
+            hour = timeInMinuets / 60;
+            minute = timeInMinuets % 60;
+
+            Console.WriteLine("hours: " + hour);
+            Console.WriteLine("minuets: " + minute);
+            Console.ReadKey();
+        }
+    }
+}
