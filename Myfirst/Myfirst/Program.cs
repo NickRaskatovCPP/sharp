fda61bfbdee98b58c9e4afa9cd33de1d51c538e7
Raskatov <cppgod@ya.ru>
2024-04-03 20:33:11 +0300

    интерполяция, конкатенация, bool type переменные

---
+++ Myfirst/Myfirst/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Myfirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Обозначение переменных, математические операции
+            //int x, y, z;
+            //x = 10;
+            //y = 25;
+            //z = x * y;
+
+            //Console.WriteLine(z);
+            //Console.ReadLine();
+
+            //булевые логические переменные
+            //bool accesIsAllowed;
+            //accesIsAllowed = 5 != 4;
+            //Console.WriteLine(accesIsAllowed);
+
+            //конкатенция (сложение строк)
+            //string greeting = 'Даров'
+            //string name = 'бро'
+            //string message = greeting + " " + name;
+            //Console.Writeline(message + ", опять ты");
+
+            //Складываем числа и строки
+            //int time = 5;
+            //Console.Writeline(5 + 5 + " завтра встречаемся в " + (time+1));
+            //получим сообщение 10 завтра встречаемся в 6
+
+            //cw tab tab = напишется console.Writeline()
+
+            //интерполяция
+            //int age = 27;
+            //string name = "Vasya"
+            //Console.Writeline("Ваше имя: " + name + " Вам " + age + "лет!");
+            //Выведет: Ваше имя Vasya Вам 27 лет!
+            //Методом интерполяции это записывается так:
+
+        }
+
+    }
+}
